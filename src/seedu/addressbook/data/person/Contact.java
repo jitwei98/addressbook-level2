2b3a5bc6b049c1a4665b160d9630c1859564bc57
jitwei98 <limjitwei@gmail.com>
2018-09-16 13:31:59 +0800

    Remove equals from Contact.java

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -25,13 +25,6 @@ public class Contact {
     }
 
     @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Contact // instanceof handles nulls
-                && this.value.equals(((Contact) other).value)); // state check
-    }
-
-    @Override
     public int hashCode() {
         return value.hashCode();
     }
